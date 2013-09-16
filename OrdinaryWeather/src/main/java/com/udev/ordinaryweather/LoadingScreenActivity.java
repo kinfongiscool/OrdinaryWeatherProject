32bd6f5c7edaef224c134df18903e783f48c2be4
Victor Ude <ude@learnovatelabs.com>
2013-09-16 00:33:12 -0500

    trying to launch a service at boot time

--- OrdinaryWeather/src/main/java/com/udev/ordinaryweather/LoadingScreenActivity.java
+++ OrdinaryWeather/src/main/java/com/udev/ordinaryweather/LoadingScreenActivity.java
@@ -1,15 +1,40 @@
 package com.udev.ordinaryweather;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.Menu;
 
 public class LoadingScreenActivity extends Activity {
+
+    private static final String TAG = "LoadingScreenActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
+
+        if (savedInstanceState == null) {
+            Intent intent = new Intent("ACTION_BOOT_COMPLETED");
+            ServiceConnection serviceConnection = new ServiceConnection() {
+                @Override
+                public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                    
+                }
+    
+                @Override
+                public void onServiceDisconnected(ComponentName componentName) {
+    
+                }
+            };
+            bindService(intent, serviceConnection, BIND_NOT_FOREGROUND);
+        } else {
+            Log.i(TAG, "Success!");
+        }
     }
 
 
--- OrdinaryWeather/src/main/java/com/udev/ordinaryweather/RequestDataService.java
+++ OrdinaryWeather/src/main/java/com/udev/ordinaryweather/RequestDataService.java
@@ -1,27 +1,51 @@
 package com.udev.ordinaryweather;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.http.AndroidHttpClient;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
-import android.widget.Toast;
+import android.util.Log;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
 import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.io.ByteArrayOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * Created by Vic on 13/9/12.
  */
 public class RequestDataService extends Service implements LocationListener {
+
+    public class BootBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Intent startServiceIntent = new Intent(context, RequestDataService.class);
+            context.startService(startServiceIntent);
+        }
+    }
+
     @Override
     public void onLocationChanged(Location location) {
-        mLocation = location;
+        requestForecastObject(location);
     }
 
     @Override
@@ -42,9 +66,11 @@ public class RequestDataService extends Service implements LocationListener {
     private Looper mServiceLooper;
     private ServiceHandler mServiceHandler;
     private LocationManager mLocationManager;
-    protected Location mLocation;
+    private JSONObject mData;
 
     private static final String TAG = "RequestDataService";
+    private final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+    private final String API_URL = "https://api.forecast.io/forecast/";
     private final String API_KEY = "5e07f9dc4b8932b18f19cea015e5512c";
 
     private final class ServiceHandler extends Handler {
@@ -54,15 +80,19 @@ public class RequestDataService extends Service implements LocationListener {
         @Override
         public void handleMessage(Message msg) {
             //todo:wait for forecast data to load
-            while (mLocation == null) {
+            while (mData == null) {
                 synchronized (this) {
                     try {
-                        wait(1000);
+                        wait(5000);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
             }
+
+            Intent intent = new Intent("DISPLAY_FORECAST");
+            intent.putExtra("data", mData.toString());
+            startActivity(intent);
 
             stopSelf(msg.arg1);
         }
@@ -78,7 +108,7 @@ public class RequestDataService extends Service implements LocationListener {
 
         try {
             mLocationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
-            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            mLocationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, this, mServiceLooper);
         } catch(Exception e) {
             e.printStackTrace();
         }
@@ -100,5 +130,43 @@ public class RequestDataService extends Service implements LocationListener {
     @Override
     public void onDestroy() {
 
+    }
+
+    private void requestForecastObject(Location location) {
+        String link = buildForecastUrl(location.getLatitude(), location.getLongitude());
+        HttpGet request = new HttpGet(link);
+        AndroidHttpClient client = AndroidHttpClient.newInstance("Android");
+        HttpResponse response;
+
+        try {
+            response = client.execute(request);
+            if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                ByteArrayOutputStream output = new ByteArrayOutputStream();
+                response.getEntity().writeTo(output);
+                output.close();
+
+                String result = output.toString();
+
+                mData = new JSONObject(result);
+                Log.i(TAG, mData.toString());
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            client.close();
+        }
+        client.close();
+    }
+
+    private String buildForecastUrl(Double latitude, Double longitude) {
+        return API_URL + API_KEY + "/" + latitude.toString() + "," + longitude.toString();
+    }
+
+    private String buildForecastUrlWithDate(Double latitude, Double longitude) {
+        Locale[] locales = DateFormat.getAvailableLocales();
+        Locale locale = (locales.length > 0) ? locales[0] : Locale.US;
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT, locale);
+        sdf.setTimeZone(TimeZone.getTimeZone(locale.toString()));
+        return API_URL + API_KEY + "/" + latitude.toString() + "," + longitude.toString() + "," + sdf.format(new Date());
     }
 }
